df45f19919423f11b262fc77f81c6df2ffe889cd
Miracle Edomwande <miracleedomwande@gmail.com>
2023-07-03 08:49:47 +0100

    added background for the chat

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { SharedModule } from 'src/app/shared/shared.module';
 import { FeatureComponent } from './feature.component';
 import { ChatModalComponent } from './chat-modal/chat-modal.component';
 
@@ -30,7 +29,6 @@ export const appRoutes: Routes = [
   declarations: [FeatureComponent],
   imports: [
     CommonModule,
-    // SharedModule,
     RouterModule.forChild(appRoutes),
   ],
 })
